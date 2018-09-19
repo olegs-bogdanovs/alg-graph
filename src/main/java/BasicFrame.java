740ea6d27e3f48e2ea8de0ecbc0b153ba6b7f75a
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2018-09-19 20:00:01 +0300

    Start Task

--- src/main/java/BasicFrame.java
+++ src/main/java/BasicFrame.java
@@ -3,8 +3,24 @@ import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 
 import javax.swing.JFrame;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BasicFrame implements GLEventListener {
+    private final static String FILE_NAME = "src/main/resources/coordinates.dat";
+    private List<Point> coordinatesList;
+    private List<Integer> codeList;
+
+    public BasicFrame() {
+        coordinatesList = new LinkedList<>();
+        codeList = new LinkedList<>();
+        readDataFromFile();
+        System.out.println(coordinatesList);
+        System.out.println(codeList);
+
+    }
 
     @Override
     public void display(GLAutoDrawable drawable) {
@@ -25,6 +41,32 @@ public class BasicFrame implements GLEventListener {
     public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
         // method body
     }
+
+    private void readDataFromFile(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+            int coordinatesCount = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < coordinatesCount; i++){
+                String[] coordinates = reader.readLine().split(" ");
+                if (coordinates.length > 2 || coordinates.length < 2) throw new Exception("Parsing Error");
+                coordinatesList.add(new Point(
+                        Integer.parseInt(coordinates[0]),
+                        Integer.parseInt(coordinates[1]))
+                );
+            }
+            int codeCount = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < codeCount; i++){
+                codeList.add(Integer.parseInt(reader.readLine()));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            codeList.clear();
+            coordinatesList.clear();
+        }
+
+    }
+
+
 
     public static void main(String[] args) {
 
